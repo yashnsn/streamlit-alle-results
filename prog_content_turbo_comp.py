f74ae776b50341564e50ee7066db2ba246d53c5e
yaswanth <yaswanth.nsn@gmail.com>
2025-08-18 19:04:10 +0530

    tops

--- prog_content_turbo_comp.py
+++ prog_content_turbo_comp.py
@@ -23,13 +23,30 @@ if selected_cats:
             metadata = json.load(f)
 
         original_image = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/cropped_garment.jpg')
-        ref_image = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/reference.jpg')
-        ref_image_upscaled = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/reference_upscaled.jpg')
-        gen_image_gen4 = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/output.png')
-        gen_image_turbo = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/output_turbo.png')
-        gen_image_turbo_refined = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/output_turbo_refined.jpg')
-        gen_image_turbo_upscaled = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/output_turbo_upscaled.jpg')
-
+        if os.path.exists(f'{root_path}/{selected_cats}/{ind_prefix}/reference.jpg'):
+            ref_image = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/reference.jpg')
+        else:
+            ref_image = None
+        if os.path.exists(f'{root_path}/{selected_cats}/{ind_prefix}/reference_upscaled.jpg'):
+            ref_image_upscaled = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/reference_upscaled.jpg')
+        else:
+            ref_image_upscaled = None
+        if os.path.exists(f'{root_path}/{selected_cats}/{ind_prefix}/output.png'):
+            gen_image_gen4 = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/output.png')
+        else:
+            gen_image_gen4 = None
+        if os.path.exists(f'{root_path}/{selected_cats}/{ind_prefix}/output_turbo.png'):
+            gen_image_turbo = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/output_turbo.png')
+        else:
+            gen_image_turbo = None
+        if os.path.exists(f'{root_path}/{selected_cats}/{ind_prefix}/output_turbo_refined.jpg'):
+            gen_image_turbo_refined = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/output_turbo_refined.jpg')
+        else:
+            gen_image_turbo_refined = None
+        if os.path.exists(f'{root_path}/{selected_cats}/{ind_prefix}/output_turbo_upscaled.jpg'):
+            gen_image_turbo_upscaled = Image.open(f'{root_path}/{selected_cats}/{ind_prefix}/output_turbo_upscaled.jpg')
+        else:
+            gen_image_turbo_upscaled = None
         st.write(ind_prefix)
         st.write(metadata)
 
@@ -43,12 +60,18 @@ if selected_cats:
         cols[6].write("Output - Gen 4 turbo(upscaled)")
         
         cols[0].image(original_image)
-        cols[1].image(ref_image)
-        cols[2].image(ref_image_upscaled)
-        cols[3].image(gen_image_gen4)
-        cols[4].image(gen_image_turbo)
-        cols[5].image(gen_image_turbo_refined)
-        cols[6].image(gen_image_turbo_upscaled)
+        if ref_image:
+            cols[1].image(ref_image)
+        if ref_image_upscaled:
+            cols[2].image(ref_image_upscaled)
+        if gen_image_gen4:
+            cols[3].image(gen_image_gen4)
+        if gen_image_turbo:
+            cols[4].image(gen_image_turbo)
+        if gen_image_turbo_refined:
+            cols[5].image(gen_image_turbo_refined)
+        if gen_image_turbo_upscaled:
+            cols[6].image(gen_image_turbo_upscaled)
 
         st.divider()
 
