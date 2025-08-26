9d1468e5448332682fe1b19b10ce8f3b5144a936
yaswanth <yaswanth.nsn@gmail.com>
2025-08-26 16:00:26 +0530

    minor changes

--- alphabke_oldvnew_v_google.py
+++ alphabke_oldvnew_v_google.py
@@ -10,7 +10,7 @@ new_alphabake_path = "multi-threaded-test_V15"
 google_results_path = "tryon_results"
 
 all_files = [i for i in os.listdir(f'{new_alphabake_path}/tryon') if not i.startswith('.')]
-
+all_files.sort()
 for ind_file in all_files:
     base_img = Image.open(f"{new_alphabake_path}/human/{ind_file}")
     garment_img = Image.open(f"{new_alphabake_path}/garment/{ind_file}")
@@ -22,8 +22,15 @@ for ind_file in all_files:
 
     cols = st.columns(5)
 
+    cols[0].write('Base image')
     cols[0].image(base_img)
+    cols[1].write('Garment image')
     cols[1].image(garment_img)
+    cols[2].write('Old alphabake tryon result')
     cols[2].image(old_alphabake_tryon)
+    cols[3].write('New alphabake tryon result')
     cols[3].image(new_alphabake_tryon)
+    cols[4].write('Google tryon result')
     cols[4].image(google_tryon)
+
+    st.divider()
