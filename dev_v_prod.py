d4d3f06f00f1dee06059f38a9add56cd83653ec8
yaswanth <yaswanth.nsn@gmail.com>
2025-09-04 00:13:20 +0530

    init

---
+++ dev_v_prod.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import os
+from PIL import Image
+import json
+
+src_root_path = "combined_images_output_V3_prod"
+src_dev_path = "combined_images_output_V3"
+
+st.set_page_config(layout='wide')
+st.title("Development vs Production")
+
+cats = [None,'fullbody','upperbody']
+
+selected_cat = st.selectbox("Select category", cats)
+if selected_cat:
+    root_path = os.path.join(src_root_path, selected_cat)
+    dev_path = os.path.join(src_dev_path, selected_cat)
+    all_folders = [i for i in os.listdir(root_path) if not i.startswith('.') and os.path.exists(os.path.join(root_path, i, 'generated_image.jpg'))]
+    
+    for ind_folder in all_folders:
+        dev_ref_upscaled = Image.open(os.path.join(dev_path, ind_folder, 'reference_compiler_upscaled.jpg'))
+        prod_ref_upscaled = Image.open(os.path.join(root_path, ind_folder, 'upscaled_reference.jpg'))
+
+        dev_cropped_garment = Image.open(os.path.join(dev_path, ind_folder, 'cropped_garment.jpg'))
+        prod_cropped_garment = Image.open(os.path.join(root_path, ind_folder, 'cropped_garment.jpg'))
+
+        dev_ref_masked = Image.open(os.path.join(dev_path, ind_folder, 'reference_altered.jpg'))
+        prod_ref_masked = Image.open(os.path.join(root_path, ind_folder, 'upscaled_reference_masked.jpg'))
+
+        dev_metadata = json.load(open(os.path.join(dev_path, ind_folder, 'metadata.json')))
+        prod_metadata = json.load(open(os.path.join(root_path, ind_folder, 'metadata.json')))
+
+        garment_image = Image.open(os.path.join(dev_path, ind_folder, 'garment.jpg'))
+        reference_image = Image.open(os.path.join(dev_path, ind_folder, 'reference.jpg'))
+
+        dev_output = Image.open(os.path.join(dev_path, ind_folder, 'output_turbo.png'))
+        prod_output = Image.open(os.path.join(root_path, ind_folder, 'generated_image.jpg'))
+
+        st.markdown(f'**{ind_folder}**')
+        st.write('dev prompt')
+        st.write(dev_metadata['image_generation_prompt'])
+        st.write('prod prompt')
+        st.write(prod_metadata['image_generation_prompt'])
+        cols = st.columns(6)
+
+        cols[0].write('Garment Image')
+        cols[0].image(garment_image)
+        cols[0].write('Reference Image')
+        cols[0].image(reference_image)
+        cols[1].write('Dev cropped garment')
+        cols[1].image(dev_cropped_garment)
+        cols[1].write('Prod cropped garment')
+        cols[1].image(prod_cropped_garment)
+        cols[2].write('Dev upscaled reference')
+        cols[2].image(dev_ref_upscaled)
+        cols[2].write('Prod upscaled reference')
+        cols[2].image(prod_ref_upscaled)
+        cols[3].write('Dev masked reference')
+        cols[3].image(dev_ref_masked)
+        cols[3].write('Prod masked reference')
+        cols[3].image(prod_ref_masked)
+        cols[4].write('Dev generated image')
+        cols[4].image(dev_output)
+        cols[5].write('Prod generated image')
+        cols[5].image(prod_output)
+
+        st.divider()
--- prog_testset_visualization.py
+++ prog_testset_visualization.py
@@ -5,38 +5,47 @@ import json
 
 st.set_page_config(layout='wide')
 
-root_path = "prog_content_testset_diverse_set_output_fin"
+root_path = "/Users/yaswanth/repositories/prog_content/prog_content_testset_diverse_set_output_fin_upscaled"
 all_cats = [i for i in os.listdir(root_path) if not i.startswith('.') and os.path.isdir(f'{root_path}/{i}')]
 all_cats = [None] + all_cats
 selected_cat = st.selectbox("Select category", all_cats)
 
 if selected_cat:
     # st.write(os.listdir(f'{root_path}/{selected_cat}'))
-    all_folders = [i for i in os.listdir(f'{root_path}/{selected_cat}') if not i.startswith('.') and os.path.exists(f'{root_path}/{selected_cat}/{i}/garment.jpg') and os.path.exists(f'{root_path}/{selected_cat}/{i}/reference.jpg') and os.path.exists(f'{root_path}/{selected_cat}/{i}/output_turbo.png')]
+    all_folders = [i for i in os.listdir(f'{root_path}/{selected_cat}') if not i.startswith('.') and os.path.exists(f'{root_path}/{selected_cat}/{i}/garment_upscaled.jpg') and os.path.exists(f'{root_path}/{selected_cat}/{i}/reference.jpg') and os.path.exists(f'{root_path}/{selected_cat}/{i}/output_turbo.png')]
     st.markdown(f"**Found {len(all_folders)} examples in category {selected_cat}**")
     for ind_folder in all_folders:
         garment_img = Image.open(f'{root_path}/{selected_cat}/{ind_folder}/garment.jpg')
+        upscaled_garment_img = Image.open(f'{root_path}/{selected_cat}/{ind_folder}/garment_upscaled.jpg')
         cropped_garment_img = Image.open(f'{root_path}/{selected_cat}/{ind_folder}/cropped_garment.jpg')
         pin_ref_img = Image.open(f'{root_path}/{selected_cat}/{ind_folder}/reference.jpg')
         masked_pin_ref_img = Image.open(f'{root_path}/{selected_cat}/{ind_folder}/reference_altered.jpg')
-        gen_img = Image.open(f'{root_path}/{selected_cat}/{ind_folder}/output_turbo_grained.png')
+        gen_img = Image.open(f'{root_path}/{selected_cat}/{ind_folder}/output_turbo.png')
+        gen_img_rerun = Image.open(f'{root_path}/{selected_cat}/{ind_folder}/output_turbo_rerun_1.png')
+        gen_img_upscaled = Image.open(f'{root_path}/{selected_cat}/{ind_folder}/output_turbo_V2.png')
         with open(f'{root_path}/{selected_cat}/{ind_folder}/metadata.json', 'r') as f:
             meta = json.load(f)
 
         # st.markdown(f"**{ind_folder} - {meta['category']}**")
         st.markdown(f'**{ind_folder}**')
         st.markdown(f"**Prompt: {meta['image_generation_prompt']}**")
-        cols = st.columns(5)
+        cols = st.columns(8)
         cols[0].write('garment image')
         cols[0].image(garment_img)
-        cols[1].write('cropped garment image')
-        cols[1].image(cropped_garment_img)
-        cols[2].write('pin reference image')
-        cols[2].image(pin_ref_img)
-        cols[3].write('masked pin reference image')
-        cols[3].image(masked_pin_ref_img)
-        cols[4].write('generated image')
-        cols[4].image(gen_img)
+        cols[1].write('garment image(upscaled)')
+        cols[1].image(upscaled_garment_img)
+        cols[2].write('cropped garment image')
+        cols[2].image(cropped_garment_img)
+        cols[3].write('pin reference image')
+        cols[3].image(pin_ref_img)
+        cols[4].write('masked pin reference image')
+        cols[4].image(masked_pin_ref_img)
+        cols[5].write('generated image')
+        cols[5].image(gen_img)
+        cols[6].write('generated image')
+        cols[6].image(gen_img_rerun)
+        cols[7].write('generated image(with garment upscaling)')
+        cols[7].image(gen_img_upscaled)
 
         st.divider()
 # all_folders = [i for i in os.listdir(root_path) if not i.startswith('.') and os.path.exists(f'{root_path}/{i}/garment.jpg') and os.path.exists(f'{root_path}/{i}/reference.jpg') and os.path.exists(f'{root_path}/{i}/generated_image.jpg') and os.path.exists(f'{root_path}/{i}/meta.json')]
