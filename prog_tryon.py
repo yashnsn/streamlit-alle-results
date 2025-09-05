7a090b12ce07d199626b7075a8d2365a6c990b7a
yaswanth <yaswanth.nsn@gmail.com>
2025-09-05 23:42:54 +0530

    new results

--- prog_tryon.py
+++ prog_tryon.py
@@ -13,7 +13,7 @@ for ind_file in all_files:
     garment_img_cropped = Image.open(f'{root_path}/{ind_file}/garment_upscaled_cropped.jpg')
     pin_ref_img = Image.open(f'{root_path}/{ind_file}/reference_upscaled.jpg')
     pin_ref_mask = Image.open(f'{root_path}/{ind_file}/reference_upscaled_mask_expanded.jpg')
-    gen_img = Image.open(f'{root_path}/{ind_file}/output_cropped_tryon.png')
+    gen_img = Image.open(f'{root_path}/{ind_file}/output_cropped_V2_tryon.png')
 
     st.markdown(f'**{ind_file}**')
     cols = st.columns(5)
