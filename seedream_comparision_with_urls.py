39732b5505852ce392e0c59cda04e57b3dc8aa7d
yaswanth <yaswanth.nsn@gmail.com>
2025-09-10 18:47:41 +0530

    fix

--- seedream_comparision_with_urls.py
+++ seedream_comparision_with_urls.py
@@ -77,7 +77,7 @@ if selected_cat:
         garment_img, pin_ref_img, gen_img, gen_img_using_upscaled, gen_img_V2 = images
 
         st.write(ind_row['generation_id'])
-        st.write("Image prompt:")
+        st.write("Image generation prompt:")
         st.write(ind_row['image_generation_prompt'])
         cols = st.columns(5)
         cols[0].write('garment image')
